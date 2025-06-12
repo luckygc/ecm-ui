304c7063ba3ebdf3379b2946bd4737fd1dc7b8ff
luckygc <chenwwecm@gmail.com>
2025-06-12 09:56:20 +0800

    c

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,21 @@
 import {createRouter, createWebHashHistory} from "vue-router";
-import {routes} from "./modules";
+import {routes} from "@/router/modules";
 import {usePageStore} from "@/stores/modules/page-store.ts";
 
-export {routes} from "./modules";
+export {routes} from "@/router/modules";
 
 export const router = createRouter({
     history: createWebHashHistory(),
     routes,
 });
 
-// 路由守卫
 router.beforeEach((_to, _from, next) => {
+    // TODO 检查登录状态
     next();
 });
 
 router.afterEach((to, from) => {
-    let routeStore = usePageStore();
-    routeStore.afterRouteChange(to,from);
+    usePageStore().afterRouteChange(to,from);
 })
 
 export default router;
