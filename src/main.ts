b4a9298d9c065ec88fde60347c6eb6f6335381ed
luckygc <chenwwecm@gmail.com>
2025-07-01 01:16:50 +0800

    优化

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import pinia from "@/store";
 import FcDesigner from '@form-create/designer';
 import formCreate from '@form-create/element-ui';
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import ElementPlus, {ElMessage} from "element-plus";
+import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import "@/assets/css/base.css";
@@ -28,5 +28,3 @@ app.use(FcDesigner)
 app.use(formCreate);
 
 app.mount("#app");
-
-ElMessage.warning('sss')
