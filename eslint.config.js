4f81efb50558a63d3f932e9db59726bbbbba917c
luckygc <chenwwecm@gmail.com>
2025-07-10 22:58:45 +0800

    移除 ESLint 配置中的 ts/method-signature-style 规则

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ export default antfu({
   formatters: true,
   vue: true,
   rules: {
-    // 'ts/method-signature-style': 'off',
     'no-console': ['warn', { allow: ['warn', 'error', 'debug'] }],
   },
   ignores: ['src/types/axios.d.ts'],
