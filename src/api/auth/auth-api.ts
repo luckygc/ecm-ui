7b4345c8acbb6e31bdf9abe235b1099ef6dede9f
luckygc <chenwwecm@gmail.com>
2025-07-13 14:45:35 +0800

    修改接口

--- src/api/auth/auth-api.ts
+++ src/api/auth/auth-api.ts
@@ -1,4 +1,3 @@
-import type { LoginForm, UserInfo } from './types'
 import { axiosInstance } from '~/utils/request/request'
 
 /**
@@ -6,7 +5,7 @@ import { axiosInstance } from '~/utils/request/request'
  * @param data 登录表单数据
  * @returns Promise<LoginResponse>
  */
-function login(data: LoginForm): Promise<{ token: string }> {
+export function login(data: LoginForm): Promise<{ token: string }> {
   return axiosInstance.postForm('/login', data)
 }
 
@@ -14,20 +13,12 @@ function login(data: LoginForm): Promise<{ token: string }> {
  * 用户登出
  * @returns Promise<void>
  */
-function logout(): Promise<void> {
+export function logout(): Promise<void> {
   return axiosInstance.post('/logout')
 }
 
-/**
- * 获取当前用户信息
- * @returns Promise<UserInfo>
- */
-function getCurrentUser(): Promise<UserInfo> {
-  return axiosInstance.get('/current-user')
-}
-
-export const authApi = {
-  login,
-  logout,
-  getCurrentUser,
+export interface LoginForm {
+  username: string
+  password: string
+  capToken: string
 }
--- src/api/user/user-api.ts
+++ src/api/user/user-api.ts
@@ -1,6 +1,22 @@
-import type { UserInfo } from '~/api/auth/types'
 import { axiosInstance } from '~/utils/request/request'
 
 export function getCurrentUserDetail(): Promise<UserInfo> {
-  return axiosInstance.get('/current-user')
+  return axiosInstance.get('/api/user/me')
+}
+
+interface CodeLabel {
+  code: string
+  label: string
+}
+
+export interface UserInfo {
+  id: number
+  username: string
+  fullName: string
+  email: string
+  mobile: string
+  status: CodeLabel
+  lastLoginTime: string
+  lastLoginIp: string
+  createTime: string
 }
